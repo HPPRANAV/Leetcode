675434d2a28051419e4d277880baeb624ac18a84
HPPRANAV <92385408+HPPRANAV@users.noreply.github.com>
2024-10-31 11:07:52 +0530

    Time: 3 ms (81.03%), Space: 16.6 MB (37.42%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.py
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -1,31 +1,27 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        time,fresh = 0, 0
-        row, col = len(grid), len(grid[0])
-        queue = deque()
+        rows, cols = len(grid), len(grid[0])
+        fresh, time = 0,0
+        queue = deque([])
 
-        for i in range(row):
-            for j in range(col):
+        for i in range(rows):
+            for j in range(cols):
                 if grid[i][j] == 1:
                     fresh+=1
                 if grid[i][j] == 2:
-                    queue.append((i,j))
+                    queue.append((i,j)) 
 
-        directions = [(0,1), (0,-1), (1,0), (-1,0)]
+        directions = [(0,-1), (-1,0),(1,0), (0,1)]
         while queue and fresh > 0:
-
             for i in range(len(queue)):
                 a,b = queue.popleft()
                 for x,y in directions:
-                    x1,y1 = a+x, b+y
-                    if(x1 < 0 or x1 == row or y1 < 0 or y1 == col or grid[x1][y1]!=1):
+                    x1,y1 = a + x, b + y
+                    if (x1 == rows or y1 == cols or x1 < 0 or y1 < 0 or grid[x1][y1]!=1):
                         continue
+                    queue.append((x1,y1))
                     grid[x1][y1] = 2
-                    queue.append((x1, y1))
                     fresh-=1
             time+=1
-        if fresh == 0:
-            return time
-        return -1
-
         
+        return time if fresh == 0 else -1

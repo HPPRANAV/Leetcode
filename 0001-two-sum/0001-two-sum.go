b46c082ed70115857aef3a8f42df9c48d8e156bd
HPPRANAV <92385408+HPPRANAV@users.noreply.github.com>
2025-02-08 20:11:34 +0530

    Time: 0 ms (100%), Space: 6.1 MB (19.34%) - LeetHub

--- 0001-two-sum/0001-two-sum.go
+++ 0001-two-sum/0001-two-sum.go
@@ -1,13 +1,13 @@
 func twoSum(nums []int, target int) []int {
     hash_set := make(map[int]int)
-    res := []int{}
+    //res := []int{}
     for i, num := range nums{
         diff := target - num
         value, ok := hash_set[diff]
         if ok{
-            res = append(res, value, i)
+           return []int{value, i}
         }
         hash_set[num] = i
     }
-    return res
+    return []int{}
 }
